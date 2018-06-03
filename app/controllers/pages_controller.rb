ef15e893154326d78e74ae638d39a201f30a4540
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-03 19:20:51 +0900

    Simple coming soon page with timer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+
+  # @emails = []
+
   def home
   end
+
+  # def newsletter
+    # @email = params["form-email"]
+
+    # if @email.save
+    #   @emails << @email
+    #   redirect_to((request.referer || '/'), :notice => "The suscription has been sent successfully.")
+    # else
+    #   redirect_to((request.referer || '/'), :alert => "Please add an valid email address.")
+    # end
+  # end
+
 end
